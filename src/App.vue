dfc9389d8beb70876a38060d4f16e30c7dc35e33
Anushamallem <104899633+Anushamallem@users.noreply.github.com>
2023-03-02 12:25:54 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <page-header :appInfo="appInfo"/>
+  <page-footer />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from '@/components/PageHeader';
+import PageFooter from './components/PageFooter.vue';
+export default {
+  name: 'App',
+  data(){
+    return{
+      appInfo : {
+        appTitle: 'Header poc',
+        appDescription: 'it is working',
+      }
+    }
+
+  },
+  components: {
+    HelloWorld,
+    PageHeader,
+    PageFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hello">
+      <h1>{{ appInfo.appTitle }}</h1><br>
+      <h1>{{ appInfo.appDescription }}</h1><br>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PageHeader',
+    props: {
+      msg: String,
+      appInfo:Object
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  
+  </style>
+  
